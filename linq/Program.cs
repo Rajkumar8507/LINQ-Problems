44c913cee2737ec9e3fe133a45b6bf16e18bac90
Rajkumar8507 <jamallarajkumar@gmail.com>
2024-11-10 16:15:19 +0530

    Add project files.

---
+++ linq/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Write a program in C# Sharp to show how the three parts of a query operation execute.
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var x = from element in array
+                    where (element % 2 == 0)
+                    select element;
+            foreach (var element in x)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+            //Write a program in C# Sharp to find the +ve numbers from a list of numbers using two where conditions in LINQ Query.
+            int[] array1 = { 1, -2, 3, -45, 6, -7, 8, -5 };
+            var y = from element in array
+                    where (element >= 0)
+                    select element;
+            foreach (var element in y)
+            {
+                Console.WriteLine(element);
+            }
+
+
+        }
+    }
+}
